97d566843db44ae6f7da294bade3bf4b05706ed2
wilbobraggins <wilbobraggins@gmail.com>
2018-09-28 07:01:34 +0000

    portfolio so far

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
  root 'pages#index'
+ get 'about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
